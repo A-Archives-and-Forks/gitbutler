b02711275153405994a53c66efbbdf8e2acc629d
Oliver Joseph Ash <oliverjash@gmail.com>
2026-05-11 13:06:43 +0100

    Extract `runOperation`, use single mutation
    Co-authored-by: Sam A. Horvath-Hunt <hello@samhh.com>

--- apps/lite/ui/src/api/mutations.ts
+++ apps/lite/ui/src/api/mutations.ts
@@ -8,50 +8,8 @@ export const applyBranchMutationOptions = mutationOptions({
 	},
 });
 
-export const absorbMutationOptions = mutationOptions({
-	mutationFn: window.lite.absorb,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
-export const commitAmendMutationOptions = mutationOptions({
-	mutationFn: window.lite.commitAmend,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
 export const commitInsertBlankMutationOptions = mutationOptions({
 	mutationFn: window.lite.commitInsertBlank,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
-export const commitMoveChangesBetweenMutationOptions = mutationOptions({
-	mutationFn: window.lite.commitMoveChangesBetween,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
-export const commitSquashMutationOptions = mutationOptions({
-	mutationFn: window.lite.commitSquash,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
-export const commitUncommitMutationOptions = mutationOptions({
-	mutationFn: window.lite.commitUncommit,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
-export const commitUncommitChangesMutationOptions = mutationOptions({
-	mutationFn: window.lite.commitUncommitChanges,
 	onSuccess: async (_data, _input, _ctx, { client }) => {
 		await client.invalidateQueries();
 	},
@@ -78,22 +36,8 @@ export const commitRewordMutationOptions = mutationOptions({
 	},
 });
 
-export const moveBranchMutationOptions = mutationOptions({
-	mutationFn: window.lite.moveBranch,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
 export const updateBranchNameMutationOptions = mutationOptions({
 	mutationFn: window.lite.updateBranchName,
-	onSuccess: async (_data, _input, _ctx, { client }) => {
-		await client.invalidateQueries();
-	},
-});
-
-export const tearOffBranchMutationOptions = mutationOptions({
-	mutationFn: window.lite.tearOffBranch,
 	onSuccess: async (_data, _input, _ctx, { client }) => {
 		await client.invalidateQueries();
 	},
