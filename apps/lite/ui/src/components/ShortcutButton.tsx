40312bc3cb46ed78cdf32248081814dea94bf1fa
Oliver Joseph Ash <oliverjash@gmail.com>
2026-05-14 07:59:43 +0100

    Allow caller to set type

--- apps/lite/ui/src/components/ShortcutButton.tsx
+++ apps/lite/ui/src/components/ShortcutButton.tsx
@@ -23,7 +23,13 @@ export const ShortcutButton: FC<
 				{...props}
 				ref={useMergedRefs(buttonRef, props.ref)}
 				// This is needed to ensure the `disabled` attribute is used.
-				render={<button type="button" disabled={props.disabled} />}
+				render={
+					<button
+						disabled={props.disabled}
+						// oxlint-disable-next-line react/button-has-type -- False positive.
+						type={props.type ?? "button"}
+					/>
+				}
 			/>
 			<Tooltip.Portal>
 				<Tooltip.Positioner sideOffset={8}>
